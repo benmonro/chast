1b46dcb3c8f1e766321cb28d2295d12317378b43
Monro, Ben <Ben.Monro@concur.com>
2018-03-09 23:28:48 -0800

    fix: build

--- build/publish_rc.sh
+++ build/publish_rc.sh
@@ -2,8 +2,8 @@
 
 set -o errexit
 
-
-npm run std-version --prerelease beta
+yarn install --production
+yarn std-version --prerelease beta
 git push --follow-tags origin ${CIRCLE_BRANCH}
 npm publish --tag prerelease
 
